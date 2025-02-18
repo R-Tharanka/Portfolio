65caa05a7d49a52555e73012bd69b993544520a0
R-Tharanka <150226449+R-Tharanka@users.noreply.github.com>
2025-02-18 18:32:33 +0530

    frontend and backend main structure

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <p>Building something awesome with MERN!</p>
+    </div>
+  );
+}
+
+export default App;
+
